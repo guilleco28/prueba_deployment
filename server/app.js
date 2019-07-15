9822fd64a273832db3101abd9fb95318746e4172
GUILLERMO_COLA <gcola@correo.um.edu.uy>
2019-07-15 18:49:48 -0300

    cambio host

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
- var http = require("http");
- var server = http.createServer(function(request, response) {
-   response.writeHead(200, {"Content-Type": "text/html"});
-   response.write("Hello!!!");
-   response.end();
-});
+// var http = require("http");
+// var server = http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/html"});
+//   response.write("Hello!!!");
+//   response.end();
+// });
 
-server.listen(8888,'localhost', function(){})
+//server.listen(8888,'192.168.0.100', function(){})
+
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('HEY!')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
